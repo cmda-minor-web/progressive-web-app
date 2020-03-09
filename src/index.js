7561c1da3491ca6ea795507062099218d9bfd2c9
Wessel Smit <wrsmit00@gmail.com>
2020-03-09 13:25:08 +0100

    succesful fetch

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ const app = express()
 const fetch = require('node-fetch')
 const ejs = require('ejs')
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
 app.listen(3000, () => console.log(`App now listening on port 3000!`))
+
+
+app.get('/', (req, res) => {
+	const api_key = "OC0EStJnYMjAhVtZl88wJjWA75lDZflYUzVmBaJ5"
+	const url = `https://api.nasa.gov/planetary/apod?api_key=${api_key}&start_date=2020-01-01`
+
+	fetch(url)
+		.then(res => res.json())
+		.then(data => res.send(data))
+})
