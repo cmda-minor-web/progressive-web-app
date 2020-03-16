67d1ca00a83124c83bb92733dc99d39e8d0d7759
TomasS <43637908+GrimReaper6667@users.noreply.github.com>
2020-03-16 15:13:05 +0100

    fixed some audit stuff, gonna test on heroku

--- static/sw.js
+++ static/sw.js
@@ -1,17 +1,20 @@
+const filesToCache = [
+    '/',
+    '/css/style.css',
+    '/js/index.js',
+    '/images/the-movie-db-logo.cb5571ba.png',
+    '/manifest-webmanifest.json',
+    '/images/no-image.svg',
+    '/images/icons-192.png',
+    '/images/icons-152.png'
+];
+
+
 self.addEventListener('install', function(e) {
     // console.log(e)
     e.waitUntil(
       caches.open('pwa-cache').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/css/style.css',
-          '/js/index.js',
-          '/images/the-movie-db-logo.cb5571ba.png',
-          '/manifest-webmanifest.json',
-          '/images/no-image.svg',
-          '/images/icons-192.png',
-          '/images/icons-512.png'
-        ]);
+        return cache.addAll(filesToCache);
       })
     );
    });
